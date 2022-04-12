c14d56bcc30791f9f74ee68f11e659ef1added75
Peter Seibel <peter@gigamonkeys.com>
2022-04-12 16:36:29 -0700

    Rejiggering parsing infrastructure a bit.

--- src/com/gigamonkeys/dungeon/Parser.java
+++ src/com/gigamonkeys/dungeon/Parser.java
@@ -7,77 +7,87 @@ interface Parser<T> {
 
   static record Parse<T>(String text, int position, boolean ok, T result) {}
 
-  static record Literal(String literal) implements Parser<String> {
-    public Parse<String> parse(String text, int position) {
-      var end = position + literal.length();
-      if (text.startsWith(literal, position) && (end == text.length() || text.charAt(end) == ' ')) {
-        return succeed(text, end, literal);
-      } else {
-        return fail(text, position);
-      }
-    }
-  }
-
-
-  static record Star<U>(Parser<U> parser) implements Parser<List<U>> {
-    public Parse<List<U>> parse(String text, int position) {
-      List<U> items = new ArrayList<>();
-      int newPosition = position;
-      while (true) {
-        Parse<U> p = parser.parse(text, newPosition);
-        if (p.ok()) {
-          items.add(p.result);
-          newPosition = p.position();
+  static Parser<String> token() {
+    return new Parser<>() {
+      public Parse<String> parse(String text, int position) {
+        var pos = position;
+        while (pos < text.length() && text.charAt(pos) != ' ') {
+          pos++;
+        }
+        if (pos > position) {
+          return succeed(text, pos, text.substring(position, pos));
         } else {
-          return succeed(text, position, items);
+          return fail(text, pos);
         }
       }
-    }
+    };
   }
 
-  static record Plus<U>(Parser<U> parser) implements Parser<List<U>> {
-    public Parse<List<U>> parse(String text, int position) {
-      if (parser.parse(text, position).ok()) {
+  static Parser<String> literal(String literal) {
+    return new Parser<>() {
+      public Parse<String> parse(String text, int position) {
+        var end = position + literal.length();
+        if (text.startsWith(literal, position) && (end == text.length() || text.charAt(end) == ' ')) {
+          return succeed(text, end, literal);
+        } else {
+          return fail(text, position);
+        }
+      }
+    };
+  }
+
+
+  static <U> Parser<List<U>> star(Parser<U> parser) {
+    return new Parser<>() {
+      public Parse<List<U>> parse(String text, int position) {
         List<U> items = new ArrayList<>();
+        int newPosition = position;
         while (true) {
-          Parse<U> p = parser.parse(text, position);
+          Parse<U> p = parser.parse(text, newPosition);
           if (p.ok()) {
             items.add(p.result);
+            newPosition = p.position();
           } else {
             return succeed(text, position, items);
           }
         }
-      } else {
-        return fail(text, position);
       }
-    }
+    };
   }
 
-  static record Or<U> (List<Parser<U>> parsers) implements Parser<U> {
-    public Parse<U> parse(String text, int position) {
-      for (var parser : parsers) {
-        var parse = parser.parse(text, position);
-        if (parse.ok()) {
-          return parse;
+  static <U> Parser<List<U>> plus(Parser<U> parser) {
+    return new Parser<>() {
+      public Parse<List<U>> parse(String text, int position) {
+        if (parser.parse(text, position).ok()) {
+          List<U> items = new ArrayList<>();
+          while (true) {
+            Parse<U> p = parser.parse(text, position);
+            if (p.ok()) {
+              items.add(p.result);
+            } else {
+              return succeed(text, position, items);
+            }
+          }
+        } else {
+          return fail(text, position);
         }
       }
-      return fail(text, position);
-    }
+    };
   }
 
-  static class Token implements Parser<String> {
-
-    public Parse<String> parse(String text, int position) {
-      var pos = position;
-      while (pos < text.length() && text.charAt(pos) != ' ') {
-        pos++;
+  @SafeVarargs
+  static <U> Parser<U> or(Parser<U>... parsers) {
+    return new Parser<>() {
+      public Parse<U> parse(String text, int position) {
+        for (var parser : parsers) {
+          var parse = parser.parse(text, position);
+          if (parse.ok()) {
+            return parse;
+          }
+        }
+        return fail(text, position);
       }
-      if (pos > position) {
-        return succeed(text, pos, text.substring(position, pos));
-      } else {
-        return fail(text, pos);
-      }
-    }
+    };
   }
 
   static <U> Parse<U> fail(String text, int position) {
--- src/com/gigamonkeys/dungeon/Test.java
+++ src/com/gigamonkeys/dungeon/Test.java
@@ -1,11 +1,12 @@
 package com.gigamonkeys.dungeon;
 
+import static com.gigamonkeys.dungeon.Parser.*;
 import java.util.*;
 
 class Test {
 
   public static void main(String[] args) {
-    var p = new Parser.Or<String>(List.of(new Parser.Literal("quux"), new Parser.Literal("foo")));
+    Parser<String> p = or(literal("quux"), literal("foo"));
     System.out.println(p.parse("foo bar baz", 0));
     System.out.println(p.parse("foo", 0));
   }
