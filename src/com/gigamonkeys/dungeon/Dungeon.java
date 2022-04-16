32ac49eb453304dd38fbeaecd9478bb998a51cbb
Peter Seibel <peter@gigamonkeys.com>
2022-04-16 11:37:23 -0700

    Start of actual command loop. Doesn't understand any commands yet.

--- src/com/gigamonkeys/dungeon/Dungeon.java
+++ src/com/gigamonkeys/dungeon/Dungeon.java
@@ -7,40 +7,51 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
+import java.util.*;
 import java.util.regex.Pattern;
 
 public class Dungeon {
 
+  private static interface Command {
+    String run(String[] args);
+  }
+
   private static final Pattern WS = Pattern.compile("\\s+");
 
   private final Player player;
+  private final BufferedReader in;
+  private final PrintStream out;
+
+  private final Map<String, Command> commands = new HashMap<>();
+
   private boolean gameOver = false;
 
-  Dungeon(Player player) {
+  Dungeon(Player player, InputStream in, PrintStream out) {
     this.player = player;
+    this.in = new BufferedReader(new InputStreamReader(in));
+    this.out = out;
   }
 
-  private void loop(InputStream in, PrintStream out) {
+  private void loop() {
     try {
-      var r = new BufferedReader(new InputStreamReader(in));
-
       out.println(player.look());
 
-      while (!gameOver()) {
-        String[] command = parse(r.readLine());
-        out.println(doCommand(command));
+      while (!gameOver) {
+        out.println(doCommand(in.readLine().toUpperCase()));
       }
     } catch (IOException ioe) {
       out.println("Yikes. Problem reading command: " + ioe);
     }
   }
 
-  public String[] parse(String line) {
-    return WS.split(line.toUpperCase());
+  public String doCommand(String line) {
+    String[] tokens = WS.split(line);
+    var c = commands.getOrDefault(tokens[0], args -> "Don't know how to " + args[0]);
+    return c.run(tokens);
   }
 
-  public boolean gameOver() {
-    return gameOver;
+  public String[] parse(String line) {
+    return WS.split(line.toUpperCase());
   }
 
   public String doCommand(String[] command) {
@@ -54,6 +65,6 @@ public class Dungeon {
     Room r2 = new Room("Second room");
     r.connect("Oaken door", r2, EAST);
     Player p = new Player(r);
-    new Dungeon(p).loop(System.in, System.out);
+    new Dungeon(p, System.in, System.out).loop();
   }
 }
