46833699b91c822824a1bc56101dd567dc19def6
Peter Seibel <peter@gigamonkeys.com>
2022-04-29 13:21:30 -0700

    Cleaning up Command interface.

--- src/com/gigamonkeys/dungeon/Command.java
+++ src/com/gigamonkeys/dungeon/Command.java
@@ -1,21 +1,20 @@
 package com.gigamonkeys.dungeon;
 
 public interface Command {
+
+  /**
+   * The verb that invokes this command.
+   */
+  public String verb();
+
+  /**
+   * Help string for this command.
+   */
+  public String help();
+
   /**
    * Run the command.
    */
   public String run(String[] args, Player p);
 
-  public String verb();
-
-  public String help();
-
-  static class SpecialOutput extends RuntimeException {
-
-    final String text;
-
-    public SpecialOutput(String text) {
-      this.text = text;
-    }
-  }
 }
