26aa3486c6f267d882d05c40f15d4611be9820f0
Peter Seibel <peter@gigamonkeys.com>
2022-04-30 18:09:09 -0700

    Cleaning up CommandParsing with new infra.

--- src/com/gigamonkeys/dungeon/CommandParser.java
+++ src/com/gigamonkeys/dungeon/CommandParser.java
@@ -25,11 +25,7 @@ public record CommandParser(Player player) {
       return value != null ? fn.apply(value) : Action.none(error);
     }
 
-    public <X> Parse<X, T> map(Function<T, X> fn) {
-      return value != null ? new Parse<>(fn.apply(value), value, null) : new Parse<>(null, value, error);
-    }
-
-    public <X> Parse<X, T> flatMap(Function<T, Optional<X>> fn) {
+    public <X> Parse<X, T> maybe(Function<T, Optional<X>> fn) {
       return value != null
         ? fn.apply(value).map(x -> new Parse<>(x, value, null)).orElse(new Parse<>(null, value, null))
         : new Parse<>(null, value, error);
@@ -50,25 +46,21 @@ public record CommandParser(Player player) {
 
   Action drop(String[] args) {
     var name = argParse(args, 1).or("Drop what?");
-    var thing = name.flatMap(n -> player.thing(n)).or(n -> "No " + n + " to drop!");
+    var thing = name.maybe(n -> player.thing(n)).or(n -> "No " + n + " to drop!");
     return thing.getAction(t -> Action.drop(player, t));
   }
 
   Action eat(String[] args) {
-    Function<String, Action> eatNamed = name ->
-      player.anyThing(name).map(food -> Action.eat(player, food)).orElse(Action.none("No " + name + " here to eat."));
-
-    return arg(args, 1).map(eatNamed).orElse(Action.none("Eat what?"));
+    var name = argParse(args, 1).or("Eat what?");
+    var thing = name.maybe(player::anyThing).or(n -> "No " + n + " here to eat.");
+    return thing.getAction(food -> Action.eat(player, food));
   }
 
   Action go(String[] args) {
-    Function<Direction, Action> forDirection = d ->
-      door(d).map(door -> Action.go(player, door)).orElse(Action.none("No door to the " + d + "."));
-
-    Function<String, Action> forName = name ->
-      direction(name).map(forDirection).orElse(Action.none("Don't understand direction " + name + "."));
-
-    return arg(args, 1).map(forName).orElse(Action.none("Go where?"));
+    var name = argParse(args, 1).or("Go where?");
+    var dir = name.maybe(Direction::fromString).or(n -> "Don't understand direction " + n + ".");
+    var door = dir.maybe(player.room()::door).or(d -> "No door to the " + d + ".");
+    return door.getAction(d -> Action.go(player, d));
   }
 
   Action look(String[] args) {
@@ -76,28 +68,17 @@ public record CommandParser(Player player) {
   }
 
   Action attack(String[] args) {
-    var target = args.length == 3 && args[1].equals("with") ? onlyMonster() : arg(args, 1).flatMap(player::roomThing);
-
-    Function<Thing, Action> attackTargetWithWeapon = t ->
-      arg(args, args.length == 3 ? 2 : 3)
-        .flatMap(player::anyThing)
-        .map(w -> Action.attack(t, w))
-        .orElse(Action.none("Attack with what?"));
-
-    Function<Thing, Action> attackWith = t ->
-      arg(args, args.length == 3 ? 1 : 2)
-        .flatMap(n -> expect("with", n))
-        .map(e -> attackTargetWithWeapon.apply(t))
-        .orElse(Action.none("Don't understand 'ATTACK' with no 'WITH'."));
-
-    return target.map(attackWith).orElse(Action.none("Attack what?"));
+    var a = argParse(args, 1).or("Attack what? And with what?");
+    var a2 = argParse(args, args.length - 1).or("Attack with what?");
+    var target = a
+      .maybe(n -> n.equals("with") ? onlyMonster() : player.roomThing(n))
+      .or(n -> "No " + n + " here to attack.");
+    var weapon = a2.maybe(player::anyThing).or(n -> "No " + n + " here to attack with!");
+    return weapon.getAction(w -> target.getAction(t -> Action.attack(t, w)));
   }
 
   Action take(String[] args) {
-    return arg(args, 1)
-      .flatMap(n -> listOfThings(args, 1))
-      .map(ts -> Action.take(player, ts))
-      .orElse(Action.none("Take what?"));
+    return parseListOfThings(args, 1).or("Take what?").getAction(ts -> Action.take(player, ts));
   }
 
   ////////////////////////////////////////////////////////////////////
@@ -107,20 +88,8 @@ public record CommandParser(Player player) {
     return idx < args.length ? new Parse<>(args[idx], args, null) : new Parse<>(null, args, null);
   }
 
-  private Optional<String> arg(String[] args, int idx) {
-    return Optional.of(idx).filter(i -> i < args.length).map(i -> args[i]);
-  }
-
-  private Optional<Direction> direction(String name) {
-    return Direction.fromString(name);
-  }
-
-  private Optional<Door> door(Direction d) {
-    return player.room().door(d);
-  }
-
-  private Optional<String> expect(String expected, String s) {
-    return Optional.of(s).filter(expected::equals);
+  private Parse<List<Thing>, String[]> parseListOfThings(String[] args, int idx) {
+    return new Parse<>(args, null, null).maybe(xs -> listOfThings(xs, idx));
   }
 
   private Optional<Thing> onlyMonster() {
